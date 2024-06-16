da53adfe12c8c53b3e37fd44915126544053149d
BogdanXoxl <bogxoxl39@gmail.com>
2024-06-16 16:18:18 +0300

    feat: 🎸 deploy postgres prod & server

--- trpc-server/src/app.ts
+++ trpc-server/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 
@@ -6,6 +7,8 @@ import { createContext } from "./context";
 import { appRouter } from "./routes";
 
 const app = express();
+
+dotenv.config();
 
 app.use(cors());
 app.use("/trpc", createExpressMiddleware({ router: appRouter, createContext }));
