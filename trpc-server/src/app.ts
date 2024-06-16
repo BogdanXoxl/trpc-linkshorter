f049e86fc6a9a9f174e19ba72c9db66dc8904527
BogdanXoxl <bogxoxl39@gmail.com>
2024-06-16 13:30:16 +0300

    test: 💍 backend

--- trpc-server/src/app.ts
+++ trpc-server/src/app.ts
@@ -9,5 +9,10 @@ const app = express();
 
 app.use(cors());
 app.use("/trpc", createExpressMiddleware({ router: appRouter, createContext }));
+app.get("/helloworld", (req, res) => {
+  res.json({
+    message: "Hello World!",
+  });
+});
 
 export { app, appRouter };
